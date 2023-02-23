e5171a8f4ef6e85a00c5380cd5d61bdeca16555e
Pierre du Toit <subaquatic.pierre@gmail.com>
2023-02-23 15:17:39 +0400

    Update port number

--- app.js
+++ app.js
@@ -111,5 +111,5 @@ app.delete("/sites/:id", (req, res) => [
 ]);
 
 app.listen(3000, () => {
-  console.log("Server started and listening on port 3000");
+  console.log("Server started and listening on port 8002");
 });
