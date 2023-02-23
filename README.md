d87cf99ee9c7097dee5127451025cba76e48a5bf
Pierre du Toit <subaquatic.pierre@gmail.com>
2023-02-23 14:57:54 +0400

    Create readme, update create view to show error on create

---
+++ README.md
@@ -0,0 +1,40 @@
+# Node Blog
+
+A Simple CRUD operation application. It allows a user to create dive sites. It leverages MongoDB, EJS templating
+and Node Express server.
+
+## Usage
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone
+```
+
+2. Install dependencies
+
+Ensure you have Mongo DB installed
+
+If not please follow the guidlines from the official Mongo website.
+
+https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+
+The Mongo Daemon should be running on at 127.0.0.1:270127
+
+```sh
+npm install
+```
+
+### Run Development Server
+
+```sh
+node app.js
+```
+
+## Technology
+
+- EJS
+- Express JS
+- MongoDB
--- app.js
+++ app.js
@@ -1,134 +1,115 @@
-const   express = require('express'),
-        app = express(),
-        mongoose = require('mongoose'),
-        bodyParser = require('body-parser'),
-        methodOverRide = require('method-override'),
-        mongoDB = 'mongodb://localhost:27017/dive_sites',
-        jquery = require('jquery');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverRide = require("method-override");
+const mongoDB = "mongodb://127.0.0.1:27017/dive_sites";
+const jquery = require("jquery");
 
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
-const SitesSchema = new mongoose.Schema (
-    {
-        title: String,
-        image: String,
-        description: String,
-        depth: Number,
-        level: String,
-        distance: String
-    }
-)
+const SitesSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  description: String,
+  depth: Number,
+  level: String,
+  distance: String,
+});
 
-const Sites = mongoose.model('Site', SitesSchema);
+const Sites = mongoose.model("Site", SitesSchema);
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded( { extended: true}));
-app.use(methodOverRide('_method'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverRide("_method"));
 
 //Routes
-app.get('/', (req,res) => {
-    res.redirect('/sites')
-})
+app.get("/", (req, res) => {
+  res.redirect("/sites");
+});
 
 //INDEX
-app.get('/sites', (req, res) => {
-    Sites.find({}, (err, allSites) => {
-        if (err){
-            res.redirect('/sites');
-        } else {
-            res.render('index', {allSites: allSites});
-        }
-    })    
-})
+
+app.get("/sites", (req, res) => {
+  Sites.find({}, (err, allSites) => {
+    if (err) {
+      res.redirect("/sites");
+    } else {
+      console.log("allSites", allSites);
+      res.render("index", { allSites: allSites });
+    }
+  });
+});
 
 //NEW
 
-app.get('/sites/new', (req, res) => {
-    res.render('new');
-})
+app.get("/sites/new", (req, res) => {
+  res.render("new");
+});
 
 //SHOW
 
-app.get('/sites/:id', (req, res) => {
-    Sites.findById(req.params.id, (err, foundSite) => {
-        if (err){
-            res.redirect('/sites');
-        } else {
-            res.render('show',{site: foundSite}); 
-        }
-    })
-})
+app.get("/sites/:id", (req, res) => {
+  Sites.findById(req.params.id, (err, foundSite) => {
+    if (err) {
+      res.redirect("/sites");
+    } else {
+      res.render("show", { site: foundSite });
+    }
+  });
+});
 
 //CREATE
 
-app.post('/sites', (req, res) => {
-    console.log(req.body.site);
-    Sites.create(req.body.site, (err) => {
-        if(err){
-            res.redirect('/sites');
-        } else {
-            res.redirect('/sites');
-        }
-    })
-})
+app.post("/sites", (req, res) => {
+  console.log(req.body.site);
+  Sites.create(req.body.site, (err) => {
+    if (err) {
+      console.log("Error creating site", err);
+      res.render("new", { error: "There was an error creating the site" });
+    } else {
+      res.redirect("/sites");
+    }
+  });
+});
 
 //EDIT
 
-app.get('/sites/:id/edit', (req,res) =>{
-    Sites.findById(req.params.id, (err, foundSite) =>{
-        if(err){
-            res.redirect('/sites');
-        } else {
-            res.render('edit', {site: foundSite})
-        }
-    })
-})
+app.get("/sites/:id/edit", (req, res) => {
+  Sites.findById(req.params.id, (err, foundSite) => {
+    if (err) {
+      res.redirect("/sites");
+    } else {
+      res.render("edit", { site: foundSite });
+    }
+  });
+});
 
 //UPDATE
 
-app.put('/sites/:id', (req,res) => {
-    Sites.findByIdAndUpdate(req.params.id, req.body.site, (err, createdSite) => {
-        if (err){
-            res.redirect('/sites');
-        } else {
-            res.redirect('/sites/' + req.params.id);
-        }
-    })
-})
+app.put("/sites/:id", (req, res) => {
+  Sites.findByIdAndUpdate(req.params.id, req.body.site, (err, createdSite) => {
+    if (err) {
+      res.redirect("/sites");
+    } else {
+      res.redirect("/sites/" + req.params.id);
+    }
+  });
+});
 
 //DESTROY
 
-app.delete('/sites/:id', (req,res) =>[
-    Sites.findByIdAndRemove(req.params.id, (err) =>{
-        if(err){
-            res.redirect('/sites');
-        } else {
-            res.redirect('/sites');
-        }
-    })
-])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.delete("/sites/:id", (req, res) => [
+  Sites.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.redirect("/sites");
+    } else {
+      res.redirect("/sites");
+    }
+  }),
+]);
 
 app.listen(3000, () => {
-    console.log('Server started and listening on port 3000')
-})
+  console.log("Server started and listening on port 3000");
+});
